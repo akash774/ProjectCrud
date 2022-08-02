9e6aeb974de771f857b0b20c144ce36eb2357909
akash774 <67888607+akash774@users.noreply.github.com>
2022-08-02 10:15:29 +0530

    Add files via upload

---
+++ Registration.aspx
@@ -0,0 +1,95 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Registration.aspx.cs" Inherits="AkashNProject.Registration" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Label ID="fullname" runat="server" Text="FullName"></asp:Label>
+            <asp:TextBox ID="txtfullname" runat="server"></asp:TextBox>
+            &nbsp;<asp:RequiredFieldValidator ID="RequiredFieldValidator1" runat="server" ControlToValidate="txtfullname" ErrorMessage="Name Is Required" ForeColor="Red"></asp:RequiredFieldValidator>
+            <br />
+            <br />
+
+            <asp:Label ID="lblmobileno" runat="server" Text="MobileNo"></asp:Label>
+            <asp:TextBox ID="txtmobileno" runat="server"></asp:TextBox>
+            <asp:RequiredFieldValidator ID="RequiredFieldValidator2" runat="server" ControlToValidate="txtmobileno" ErrorMessage="MobileNo Is Required" ForeColor="Red"></asp:RequiredFieldValidator>
+            <br />
+            <br />
+
+            <asp:Label ID="lblemail" runat="server" Text="EmailID"></asp:Label>
+            <asp:TextBox ID="txtemail" runat="server"></asp:TextBox>
+            <asp:RequiredFieldValidator ID="RequiredFieldValidator3" runat="server" ControlToValidate="txtemail" ErrorMessage="Email Is Required" ForeColor="Red"></asp:RequiredFieldValidator>
+            <br />
+            <br />
+
+            <asp:Label ID="lblcomapny" runat="server" Text="CompanyName"></asp:Label>
+            <asp:TextBox ID="txtcomapny" runat="server"></asp:TextBox>
+            <asp:RequiredFieldValidator ID="RequiredFieldValidator4" runat="server" ControlToValidate="txtcomapny" ErrorMessage="Company Name Is Mandatory" ForeColor="Red"></asp:RequiredFieldValidator>
+            <br />
+            <br />
+
+            <asp:Button ID="btnsubmit" runat="server" Text="Submit" OnClick="btnsubmit_Click" />
+
+            <asp:Repeater ID="Repeater1" runat="server">
+                <ItemTemplate>
+                    <table>
+                        <tr>
+                            <tr>
+                                <td>EmployeeId</td>
+                                <td>
+                                    <asp:Label ID="id" runat="server" Text='<%#Eval("EmpID")%>' />
+                                </td>
+                            </tr>
+                        </tr>
+
+                        <tr>
+                            <td>FullName</td>
+                            <td>
+                                <asp:Label ID="lblfullname" runat="server" Text='<%#Eval("FullName")%>' />
+                                <asp:TextBox ID="txtfullname" runat="server" Text='<%#Eval("FullName")%>' Visible="false" />
+                            </td>
+                        </tr>
+
+                        <tr>
+                            <td>MobileNo</td>
+                            <td>
+                                <asp:Label ID="lblmobileno" runat="server" Text='<%#Eval("MobileNo")%>' />
+                                <asp:TextBox ID="txtmobileno" runat="server" Text='<%#Eval("MobileNo")%>' Visible="false" />
+                            </td>
+                        </tr>
+
+                        <tr>
+                            <td>EmailId</td>
+                            <td>
+                                <asp:Label ID="lblemail" runat="server" Text='<%#Eval("EmailId")%>' />
+                                <asp:TextBox ID="txtemail" runat="server" Text='<%#Eval("EmailId")%>' Visible="false" />
+                            </td>
+                        </tr>
+
+                        <tr>
+                            <td>CompanyName</td>
+                            <td>
+                                <asp:Label ID="lblcompany" runat="server" Text='<%#Eval("CompanyName")%>' />
+                                <asp:TextBox ID="txtcompany" runat="server" Text='<%#Eval("CompanyName")%>' Visible="false" />
+                            </td>   
+                        </tr>
+
+
+
+
+                    </table>
+                    <asp:LinkButton ID="linedit" runat="server" OnClick="linedit_Click">Edit</asp:LinkButton>
+                    <asp:LinkButton ID="lknupdate" runat="server" OnClick="lknupdate_Click">Update</asp:LinkButton>
+                    <asp:LinkButton ID="lkndelete" runat="server" OnClick="lkndelete_Click">Delete</asp:LinkButton>
+
+                </ItemTemplate>
+            </asp:Repeater>
+        </div>
+    </form>
+</body>
+</html>
---
+++ Registration.aspx.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace AkashNProject
+{
+    public partial class Registration : System.Web.UI.Page
+    {
+       // SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["dbconnection"].ConnectionString);
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                Bindrepeater();
+            }
+        }
+
+        protected void btnsubmit_Click(object sender, EventArgs e)
+        {
+            string cs = ConfigurationManager.ConnectionStrings["dbconnection"].ConnectionString;
+            using (SqlConnection con = new SqlConnection(cs))
+            {
+                SqlCommand cmd = new SqlCommand("sp_insert", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@FullName", txtfullname.Text);
+                cmd.Parameters.AddWithValue("@MobileNo", txtmobileno.Text);
+                cmd.Parameters.AddWithValue("@EmailId", txtemail.Text);
+                cmd.Parameters.AddWithValue("@CompanyName", txtcomapny.Text);
+
+                con.Open();
+                cmd.ExecuteNonQuery();
+                con.Close();
+                Bindrepeater();
+            }
+        }
+        protected void Bindrepeater()
+        {
+            string cs = ConfigurationManager.ConnectionStrings["dbconnection"].ConnectionString;
+            using (SqlConnection con = new SqlConnection(cs))
+            {
+                SqlCommand cmd = new SqlCommand("getdata", con);
+                con.Open();
+                Repeater1.DataSource = cmd.ExecuteReader();
+                Repeater1.DataBind();
+
+            }
+        }
+
+       
+       
+        protected void lknupdate_Click(object sender, EventArgs e)
+        {
+                RepeaterItem item = (sender as LinkButton).Parent as RepeaterItem;
+                int ID = int.Parse((item.FindControl("id") as Label).Text);
+                string NAME = (item.FindControl("txtfullname") as TextBox).Text.Trim();
+                string MOBILE = (item.FindControl("txtmobileno") as TextBox).Text.Trim();
+                //int MOBILE = int.Parse((item.FindControl("txtmobileno") as TextBox).Text);
+                string Email = (item.FindControl("txtemail") as TextBox).Text.Trim();
+                string Company = (item.FindControl("txtcompany") as TextBox).Text.Trim();
+                using (SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["dbconnection"].ConnectionString))
+                {
+                    SqlCommand cmd = new SqlCommand("[dbo].[sp_update]", con);
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("@EmpID", ID);
+                    cmd.Parameters.AddWithValue("@FullName", NAME);
+                    cmd.Parameters.AddWithValue("@MobileNo", MOBILE);
+                    cmd.Parameters.AddWithValue("@EmailId", Email);
+                    cmd.Parameters.AddWithValue("@CompanyName", Company);
+                    con.Open();
+                    cmd.ExecuteNonQuery();
+                    con.Close();
+                    Bindrepeater();
+
+                }
+            }
+
+        protected void linedit_Click(object sender, EventArgs e)
+        {
+            RepeaterItem item = (sender as LinkButton).Parent as RepeaterItem;
+            this.ToggleElements(item, true);
+            this.ToggleElements1(item, false);
+        }
+
+         private void ToggleElements(RepeaterItem item, bool isEdit)
+            {
+                
+                item.FindControl("lknupdate").Visible = isEdit;
+                item.FindControl("txtfullname").Visible = isEdit;
+                item.FindControl("txtmobileno").Visible = isEdit;
+                item.FindControl("txtemail").Visible = isEdit;
+                item.FindControl("txtcompany").Visible = isEdit;
+                
+
+            }
+
+            private void ToggleElements1(RepeaterItem item, bool isEdit)
+            {
+                item.FindControl("linedit").Visible = isEdit;
+                item.FindControl("lblfullname").Visible = isEdit;
+                item.FindControl("lblmobileno").Visible = isEdit;
+                item.FindControl("txtemail").Visible = isEdit;
+                item.FindControl("lblcompany").Visible = isEdit;
+
+            }
+
+        protected void lkndelete_Click(object sender, EventArgs e)
+        {
+            int ID = int.Parse(((sender as LinkButton).NamingContainer.FindControl("id") as Label).Text);
+            string constr = ConfigurationManager.ConnectionStrings["dbconnection"].ConnectionString;
+            using (SqlConnection con = new SqlConnection(constr))
+            {
+                SqlCommand cmd = new SqlCommand("sp_delete", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                cmd.Parameters.AddWithValue("@EmpID", ID);
+                con.Open();
+                cmd.ExecuteNonQuery();
+                con.Close();
+                Bindrepeater();
+            }
+        }
+    }
+ }
+
